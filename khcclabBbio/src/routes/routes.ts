c1448f36f153132000f9dbc268a45301d60b2bec
Mustafa <mmalowisi19@cit.just.edu.jo>
2023-12-18 16:23:28 +0300

    apply imports

---
+++ khcclabBbio/src/routes/routes.ts
@@ -0,0 +1,12 @@
+import Login from "@pages/login";
+import React from "react";
+interface route {
+  path: string;
+  component: React.ReactElement;
+}
+[];
+const routesURl: route[] = [
+  { path: "ddd", component: <Login /> },
+  { path: "ddd", component: <Login /> },
+  { path: "ddd", component: <Login /> },
+];
