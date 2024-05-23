a5bf0dfb98c6f71ace4984af9fc2126e41743d1c
Mustafa <mustafaalowisi@gmail.com>
2024-05-23 22:44:23 +0300

    fix add sample form

--- khcclab/src/pages/nursing/AddSamples.tsx
+++ khcclab/src/pages/nursing/AddSamples.tsx
@@ -4,10 +4,8 @@ import CircularProgress from "@mui/material/CircularProgress";
 import { AddSamplesForm } from "Components/Patient/AddSamplesForm";
 import { MainLayout } from "UI/MainLayout";
 import { ScrollableContainer } from "UI/ScrollableContainer";
-import { set } from "date-fns";
-import { useData } from "hooks/useData";
 import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { onSignOutSuccess } from "services/authService";
@@ -20,16 +18,7 @@ export const AddSamples = () => {
   const dispatch = useDispatch();
   const { patientId } = useParams();
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
-  const [formsData, setFormsData] = useState<Array<any>>([
-    {
-      containerType: "",
-      drawnAt: "",
-      numberOfSamples: "",
-      sampleSerial: "",
-      sampleType: "",
-      storageType: "",
-    },
-  ]);
+
   useEffect(() => {
     getPatientData();
   }, [patientId]);
@@ -60,39 +49,28 @@ export const AddSamples = () => {
     control,
     handleSubmit,
     formState: { errors, isValid },
-    getValues,
-    setValue,
     reset,
   } = useForm({
     resolver: yupResolver(sampleSchema),
     defaultValues: {
-      samples: [...formsData],
+      samples: [
+        {
+          containerType: "",
+          drawnAt: "",
+          numberOfSamples: "",
+          sampleSerial: "",
+          sampleType: "",
+          storageType: "",
+        },
+      ],
     },
   });
 
-  const handleAddForm = () => {
-    const newFormData = {
-      containerType: "",
-      drawnAt: "",
-      numberOfSamples: "",
-      sampleSerial: "",
-      sampleType: "",
-      storageType: "",
-    };
-    setFormsData([...formsData, newFormData]);
-    const currentDefaultValues = getValues();
-    const currentSamples = currentDefaultValues.samples as
-      | {
-          sampleType: string;
-          containerType: string;
-          numberOfSamples: string;
-          drawnAt: string;
-          sampleSerial: string;
-          storageType: string;
-        }[]
-      | undefined;
-    setValue("samples", [...(currentSamples || []), newFormData]);
-  };
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "samples",
+    rules: { minLength: 1 },
+  });
 
   const onSubmit = async (data: any) => {
     let patientIdUser = patientId?.replace(/^:(.*)$/, "$1")!;
@@ -110,16 +88,6 @@ export const AddSamples = () => {
           severity: "success",
         },
       });
-      setFormsData([
-        {
-          containerType: "",
-          drawnAt: "",
-          numberOfSamples: "",
-          sampleSerial: "",
-          sampleType: "",
-          storageType: "",
-        },
-      ]);
       reset();
     } catch (err: any) {
       dispatch({
@@ -137,33 +105,30 @@ export const AddSamples = () => {
     }
   };
 
-  const handleDeleteForm = (index: number) => {
-    const updatedFormsData = formsData.filter((_, i) => i !== index);
-    setFormsData(updatedFormsData);
-    const currentDefaultValues = getValues();
-    const updatedFormsDefault = currentDefaultValues.samples?.filter(
-      (_, i) => i !== index
-    );
-    setValue("samples", updatedFormsDefault);
-  };
-
   return (
     <div className="w-full h-full flex flex-col gap-3">
       <span className="text-2xl font-bold">Add Samples</span>
       <ScrollableContainer>
-        {formsData.map((_, index: number) => {
-          return (
-            <AddSamplesForm
-              key={index}
-              control={control}
-              errors={errors}
-              index={index}
-              onClick={handleAddForm}
-              remove={handleDeleteForm}
-              formLength={formsData.length}
-            />
-          );
-        })}
+        {fields.map((field, index) => (
+          <AddSamplesForm
+            key={field.id}
+            control={control}
+            errors={errors}
+            index={index}
+            onClick={() =>
+              append({
+                containerType: "",
+                drawnAt: "",
+                numberOfSamples: "",
+                sampleSerial: "",
+                sampleType: "",
+                storageType: "",
+              })
+            }
+            remove={remove}
+            formLength={fields.length}
+          />
+        ))}
       </ScrollableContainer>
 
       <Button
