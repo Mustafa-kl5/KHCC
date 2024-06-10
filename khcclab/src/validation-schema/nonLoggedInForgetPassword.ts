016b6ad41ab11fa19d2399433e1bcfc562453b7e
Mustafa Mahmood <91684213+Mustafa-kl5@users.noreply.github.com>
2024-06-10 12:21:11 +0300

    apply for get password

---
+++ khcclab/src/validation-schema/nonLoggedInForgetPassword.ts
@@ -0,0 +1,15 @@
+import { PASSWORD_PATTERN } from "utils/constant";
+import { object, string, ref } from "yup";
+
+export const registrationSchema = object({
+  email: string()
+    .required("email is required!")
+    .email("please enter a valid email"),
+  password: string()
+    .required("password is required!")
+    .matches(PASSWORD_PATTERN, "the password at least should be 8 characters"),
+  confirmPassword: string()
+    .required("confirm the password please")
+    .oneOf([ref("password")], "password does not match!")
+    .matches(PASSWORD_PATTERN, "the password at least should be 8 characters"),
+}).required();
